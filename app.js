d5be872ff32aa00b82d5035884bad3caabd02132
gowthampenjarla <penjarlagowtham@gmail.com>
2020-01-11 19:27:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.connection.on('error', (err) => {
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT ||3000;
 
 // Routing
 const categories = require('./routes/categories');
